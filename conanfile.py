e526d69eedb381b0ddae80c798459686760a17c8
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2024-09-01 22:00:31 +0200

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,44 @@
+from conans import ConanFile, CMake, tools
+import os
+import glob
+
+
+class Slideio(ConanFile):
+    name = "slideio"
+    url = ""
+    version = "2.7.0"
+    description = "SlideIO library."
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    license = "BSD 3-Clause"
+    _source_subfolder = "source_subfolder"
+
+    def source(self):
+        git = tools.Git()
+        git.clone("https://github.com/Booritas/slideio.git", "refactoring", args="--recursive")
+
+    def build(self):
+        configuration = 'release'
+        if self.settings.build_type == 'Debug':
+            configuration = 'debug'
+        self.run(f"python install.py -a install -c {configuration}")
+
+    def package(self):
+        os_name = str(self.settings.os)
+        build_type = str(self.settings.build_type)
+        install_folder = os.path.join("install", os_name)
+        print(f"install_folder: {install_folder}")
+        self.copy(pattern="*.hpp", dst="slideo", src=os.path.join(install_folder,"sldieio"))
+        lib_pattern = "*.lib"
+        if self.settings.os == "Linux":
+            lib_pattern = "*.so*"
+        self.copy(pattern=lib_pattern, dst="lib", src=os.path.join(install_folder, "lib"))
+        bin_pattern = "*.dll"
+        if self.settings.os == "Linux":
+            lib_pattern = "*.so*"
+        self.copy(pattern=bin_pattern, dst="bin", src=os.path.join(install_folder, "bin"))
+
+    def package_info(self):
+        self.cpp_info.includedirs = ["slideio"]
+        self.cpp_info.libdirs = ["lib"]
+        self.cpp_info.bindirs = ["bin"]
