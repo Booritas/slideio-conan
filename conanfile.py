80f78fb9d2749afc56ef9c16144f62ba65430c65
Stanislav Melnikov <s.melnikov@smart-reporting.com>
2024-10-12 12:41:40 +0200

    macos build

--- conanfile.py
+++ conanfile.py
@@ -25,24 +25,54 @@ class Slideio(ConanFile):
 
     def package(self):
         os_name = str(self.settings.os)
+        os_dir = os_name
+        if os_name == "Macos":
+            os_dir = "OSX"
         build_type = str(self.settings.build_type)
-        install_folder = os.path.join("install", os_name)
+        install_folder = os.path.join("install", os_dir)
         print(f"install_folder: {install_folder}")
         self.copy(pattern="*.hpp", dst="include", src=os.path.join(install_folder,"include"))
         lib_pattern = "*.lib"
-        if self.settings.os == "Linux":
-            lib_pattern = "*.so*"
-        self.copy(pattern=lib_pattern, dst="lib", src=os.path.join(install_folder, "lib"))
         bin_pattern = "*.dll"
         if self.settings.os == "Linux":
             lib_pattern = "*.so*"
+            bin_pattern = "*.so*"
+        elif self.settings.os == "Macos":
+            lib_pattern = "*.dylib"
+            bin_pattern = "*.dylib"
+            
+        self.copy(pattern=lib_pattern, dst="lib", src=os.path.join(install_folder, "lib"))
         self.copy(pattern=bin_pattern, dst="bin", src=os.path.join(install_folder, "bin"))
+        libs = self.get_shared_libs()
+
+        for lib in libs:
+            # copy distributed shared libraries to lib folder
+            self.copy(pattern=lib, dst="lib", src=os.path.join(install_folder, "bin"))
+
+        # if self.settings.os == "Linux" or self.settings.os == "Macos":
+        #     # copy shared libraries to bin folder
+        #     for lib in libs:
+        #         self.copy(pattern=lib, dst="bin", src=os.path.join(install_folder, "lib"))
+
+    def get_shared_libs(self):
+        lib_names = ["slideio", "slideio-converter", "slideio-transformer", "slideio-core"]
+        lib_prefix = ""
+        if self.settings.build_type == 'Debug':
+            for i in range (len(lib_names)):
+                lib_names[i] += "_d"
+        lib_suffix = ".lib"
+        if self.settings.os == "Linux":
+            lib_suffix = ".so"
+            lib_prefix = "lib"
+        elif self.settings.os == "Macos":
+            lib_suffix = ".dylib"
+            lib_prefix = "lib"
+        for i in range (len(lib_names)):
+            lib_names[i] = lib_prefix + lib_names[i] + lib_suffix
+        return lib_names
 
     def package_info(self):
         self.cpp_info.includedirs = ["include"]
         self.cpp_info.libdirs = ["lib"]
         self.cpp_info.bindirs = ["bin"]
-        if self.settings.build_type == 'Debug':
-            self.cpp_info.libs = ["slideio_d.lib","slideio-converter_d.lib","slideio-transformer_d.lib", "slideio-core_d.lib"]
-        else:
-            self.cpp_info.libs = ["slideio.lib","slideio-converter.lib","slideio-transformer.lib", "slideio-core.lib"]
+        self.cpp_info.libs = self.get_shared_libs()
